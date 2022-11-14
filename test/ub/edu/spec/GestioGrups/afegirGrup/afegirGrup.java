51ff7bdb30dfb5e51896559e6dfb0f6bc2d3374d
polfa <polferfer@gmail.com>
2022-11-14 15:55:53 +0100

    He implementat i testejat el test del cas d'us 14: Calcular Ranking Grup

--- test/ub/edu/spec/GestioGrups/afegirGrup/afegirGrup.java
+++ test/ub/edu/spec/GestioGrups/afegirGrup/afegirGrup.java
@@ -4,8 +4,6 @@ import org.concordion.api.BeforeExample;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 import ub.edu.controller.Controller;
-import ub.edu.model.Grup;
-import ub.edu.model.Persona;
 
 @RunWith(ConcordionRunner.class)
 public class afegirGrup {
