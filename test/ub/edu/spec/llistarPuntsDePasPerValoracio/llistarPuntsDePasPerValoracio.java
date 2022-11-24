b6d00436af7e02aa98e84d2b6bf2325864cc8512
Tomas Aladjem <samot2003@gmail.com>
2022-11-24 23:27:39 +0100

    He afegit coses al test d'acceptacio de la US:16 Llistar punts de pas per valoracio

--- test/ub/edu/spec/llistarPuntsDePasPerValoracio/llistarPuntsDePasPerValoracio.java
+++ test/ub/edu/spec/llistarPuntsDePasPerValoracio/llistarPuntsDePasPerValoracio.java
@@ -1,9 +1,26 @@
 package ub.edu.spec.llistarPuntsDePasPerValoracio;
 
+import org.concordion.api.BeforeExample;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
+import ub.edu.controller.Controller;
 
 @RunWith(ConcordionRunner.class)
 public class llistarPuntsDePasPerValoracio {
+
+    private Controller controlador;
+
+    @BeforeExample
+    private void init() {
+        controlador = new Controller();
+    }
+
+    public Iterable<String> llistarPuntsDePasValoracio (String nomRuta) {
+         return controlador.llistarPuntsDePasRuta(nomRuta);
+    }
+
+    public void initRuta(){
+        controlador.iniciarRuta("Terres de l'Ebre");
+    }
 
 }
